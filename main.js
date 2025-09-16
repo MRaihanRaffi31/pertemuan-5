bb5156de2c3d68bb2f06794d0c34d9b5107733b4
MRaihanRaffi31 <m.raihanraffi31@gmail.com>
2025-09-16 16:04:52 +0800

    tambah info

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const mainText = document.getElementById("main-text");
 mainText.innerHTML = "Halo Pendikom FKIP UNMUL";
+
+class Mahasiswa {
+    constructor(nama, nim, jurusan) {
+        this.nama = nama;
+        this.nim = nim;
+        this.angkatan = angkatan;
+
+    }}
