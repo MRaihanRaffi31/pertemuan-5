5aeaca20cec54c69fa012dd6ff8acd8f8c67d80e
MRaihanRaffi31 <m.raihanraffi31@gmail.com>
2025-09-16 16:39:21 +0800

    tambah info

--- main.js
+++ main.js
@@ -13,9 +13,14 @@ class Bank {
             alert("Saldo tidak cukup");
         } else {
             this.saldo = this.saldo - jumlah_uang;
+        }          
+                    
         }
+        cekSaldo() {
+            return`Nama: ${this.nama} \n Saldo: ${this.saldo}`;
+        }       
     }
-}
+
 let bank = new Bank("Rehan", 1000000);
 bank.Deposit(500000);
 bank.tarik(2000000);
