f62e0a507ad6405f87ad3bd91c6ccf9cd4b88e27
MRaihanRaffi31 <m.raihanraffi31@gmail.com>
2025-09-16 16:56:41 +0800

    tambah info

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 const mainText = document.getElementById("main-text");
 class Bank {
-    constructor(nama, saldo) {
-        this.nama = nama;
-        this.saldo = saldo;
-    }
+  constructor(nama, saldo) {
+    this.nama = nama;
+    this.saldo = saldo;
+  }
 
-    Deposit(jumlah_uang) {
-        this.saldo = this.saldo +  jumlah_uang;
+  Deposit(jumlah_uang) {
+    this.saldo = this.saldo + jumlah_uang;
+  }
+  tarik(jumlah_uang) {
+    if (jumlah_uang > this.saldo) {
+      alert("Saldo tidak cukup");
+    } else {
+      this.saldo = this.saldo - jumlah_uang;
     }
-    tarik(jumlah_uang) {
-        if (jumlah_uang > this.saldo) {
-            alert("Saldo tidak cukup");
-        } else {
-            this.saldo = this.saldo - jumlah_uang;
-        }          
-                    
-        }
-        cekSaldo() {
-            return `Nama: ${this.nama}\n 
+  }
+  cekSaldo() {
+    return `Nama: ${this.nama}\n 
                     Sisa Saldo: ${this.saldo}`;
-        }       
-    }
+  }
+}
 
 let bank = new Bank("Rehan", 1000000);
 
