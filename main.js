4dffcfa63ff3332f59291fd8b5ad4cbffeb3f20e
MRaihanRaffi31 <m.raihanraffi31@gmail.com>
2025-09-16 16:33:56 +0800

    tambah info

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ class Bank {
     Deposit(jumlah_uang) {
         this.saldo = this.saldo +  jumlah_uang;
     }
+    tarik(jumlah_uang) {
+        if (jumlah_uang > this.saldo) {
+            alert("Saldo tidak cukup");
+        } else {
+            this.saldo = this.saldo - jumlah_uang;
+        }
+    }
 }
 let bank = new Bank("Rehan", 1000000);
 bank.Deposit(500000);
